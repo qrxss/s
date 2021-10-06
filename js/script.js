b395b70af76d1b543d1434dcf0eb5b12ef7a6a5a
qrxss <91896362+qrxss@users.noreply.github.com>
2021-10-06 12:58:39 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,25 +20,25 @@ function cookieClick() {
     var upgradeLevel = document.getElementById("upgradeLevel");
     
     numbers.innerHTML = num;      
-    //automatic Granny upgrade to 2x
+    //automatic Old Cat upgrade to 2x
     if(num >= 30 ){
         num += 2;
         upgradeLevel.innerHTML = "Old Cat Level";
     }
 
-    //automatic factory upgrade to 10x
+    //automatic Super Cat upgrade to 10x
     if(num >= 500) {
         num += 10;
         upgradeLevel.innerHTML = "Super Cat Level";
     }
 
-    //automatic plant upgrade to 30x
+    //automatic Cat Nip Level upgrade to 30x
     if(num >= 1000) {
         num += 30;
         upgradeLevel.innerHTML = "Cat Nip Level";
     }
 
-    //automatic super factory upgrade to 1000x
+    //automatic Super Cat NIp upgrade to 1000x
     if(num >= 100000) {
         num += 1000;
         upgradeLevel.innerHTML = "Super Cat Nip Level";
